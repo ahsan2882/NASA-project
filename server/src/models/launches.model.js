8ee487321129325ca294e3127aec33b420422777
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-23 19:04:26 +0500

    Update launches.model.js

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,7 +5,7 @@ const launch = {
     mission: 'Hubble',
     rocket: 'Rover',
     launchDate: new Date('April 1 2069'),
-    destination: 'Moon',
+    destination: 'Kepler-1652 b',
     customers: ['NASA', 'NOAA', 'Elon Musk'],
     upcoming: true,
     success: true
@@ -13,6 +13,10 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
 module.exports = {
-    launches
+    getAllLaunches
 };
