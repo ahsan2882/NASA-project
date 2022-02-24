712fe69e846f0de020f90e80b4328cbec04f7943
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-24 11:15:54 +0500

    Update launches.controller.js

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,8 +6,18 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
-
-    launch.launchDate = new Date(launch.launchDate);
+    if (!launch.launchDate || !launch.mission || !launch.rocket || !launch.destination) {
+        return res.status(400).json({
+            error: 'Missing required launch property'
+        })
+    }
+    launch.launchDate = new Date(launch.launchDate).valueOf();
+    console.log(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid Date'
+        })
+    }
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
