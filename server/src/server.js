450cf2286765dbbb3f4aac95e297b5042b0364bc
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-26 19:17:46 +0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -10,11 +10,9 @@ const server = http.createServer(app);
 
 async function startServer() {
     await loadPlanetsData();
-    
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
     });
 }
-
 startServer();
 
