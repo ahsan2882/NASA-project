65bce545b617d675b2ee25bc2512fd3cfc38011c
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-27 22:27:55 +0500

    update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,27 +1,21 @@
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./app');
 
 const {loadPlanetsData} = require('./models/planets.model');
+const { connectToMongoDB } = require('./services/mongo');
 
 const PORT = Number(process.env.PORT) || 8000;
 // FtTb0vCqbdhwy2KR
 // nasa-api
 
 
-const MONGO_URL = 'mongodb+srv://nasa-api:FtTb0vCqbdhwy2KR@nasacluster.xnlan.mongodb.net/nasaDB?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
-});
 
-mongoose.connection.on('error', (err) => {
-    console.error(`Error: ${err}`);
-});
 
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    await connectToMongoDB();
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
