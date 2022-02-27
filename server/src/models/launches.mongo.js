f91a0236039ba263c509fa32149a63b5f6fb3733
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-27 10:53:23 +0500

    Create launches.mongo.js

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+    },
+    launchDate: {
+        type: Date,
+        required: true,
+    },
+    mission: {
+        type: String,
+        required: true,
+    },
+    rocket: {
+        type: String,
+        required: true,
+    },
+})
