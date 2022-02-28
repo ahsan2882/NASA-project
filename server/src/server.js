84c9e2d81750d01d1dd754e316c571218f3064fc
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-02-28 15:09:43 +0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const {loadPlanetsData} = require('./models/planets.model');
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchesData } = require('./models/launches.model');
 const { connectToMongoDB } = require('./services/mongo');
 
 const PORT = Number(process.env.PORT) || 8000;
@@ -17,6 +18,8 @@ const server = http.createServer(app);
 async function startServer() {
     await connectToMongoDB();
     await loadPlanetsData();
+    await loadLaunchesData();
+
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
     });
