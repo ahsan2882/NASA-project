8dae32675c80aae90443f271c2490569d33dd917
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-06 19:49:03 +0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -10,9 +10,7 @@ const app = express();
 
 app.use(morgan('combined'));
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')))
