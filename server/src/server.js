4583c417322cbf1c7d34fdd7aa3f68125d7ff386
Ahsan Shahid <ahsanshahid2882@gmail.com>
2022-03-06 19:49:04 +0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -9,14 +9,8 @@ const { loadLaunchesData } = require('./models/launches.model');
 const { connectToMongoDB } = require('./services/mongo');
 
 const PORT = Number(process.env.PORT) || 8000;
-// FtTb0vCqbdhwy2KR
-// nasa-api
-
-
 
 const server = http.createServer(app);
-
-
 
 async function startServer() {
     await connectToMongoDB();
